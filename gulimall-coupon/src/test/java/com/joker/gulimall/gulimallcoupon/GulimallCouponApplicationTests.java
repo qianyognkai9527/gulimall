7c0e5ac416b84cee2c14c7ffbe583cb5edf8281a
joker <2101418065@qq.com>
2026-04-23 00:14:24 +0800

    初始化项目

---
+++ gulimall-coupon/src/test/java/com/joker/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joker.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
